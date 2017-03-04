bae8c5f1f09c1f592736d79a693a0e190408bb97
Toshit Panigrahi <toshitpanigrahi@Toshits-MacBook-Pro-2.local>
2017-03-04 14:57:23 -0500

    Sign In XML done

---
+++ app/src/main/java/com/heybotler/botlerapp/FontChanger.java
@@ -0,0 +1,24 @@
+package com.heybotler.botlerapp;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+/**
+ * Created by toshitpanigrahi on 3/4/17.
+ */
+
+public class FontChanger {
+    public static void changeFont(AssetManager am, TextView tv, String weight) {
+        if (weight.equals("bold")) {
+            Typeface custom_font = Typeface.createFromAsset(am,  "fonts/OpenSans-Bold.ttf");
+            tv.setTypeface(custom_font);
+        } else if (weight.equals("light")) {
+            Typeface custom_font = Typeface.createFromAsset(am,  "fonts/OpenSans-Light.ttf");
+            tv.setTypeface(custom_font);
+        } else {
+            Typeface custom_font = Typeface.createFromAsset(am,  "fonts/OpenSans-Regular.ttf");
+            tv.setTypeface(custom_font);
+        }
+    }
+}
--- app/src/main/java/com/heybotler/botlerapp/SignInActivity.java
+++ app/src/main/java/com/heybotler/botlerapp/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.heybotler.botlerapp;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_in);
+
+        TextView tv = (TextView) findViewById(R.id.login_title);
+        AssetManager am = getApplicationContext().getAssets();
+        FontChanger.changeFont(am, tv, "bold");
+
+        tv = (TextView) findViewById(R.id.login_title_text);
+        FontChanger.changeFont(am, tv, "regular");
     }
 }
