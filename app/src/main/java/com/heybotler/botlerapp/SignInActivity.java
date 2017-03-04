aae6732aece9d1896680fa72413f5b291949575f
Toshit Panigrahi <toshitpanigrahi@Toshits-MacBook-Pro-2.local>
2017-03-04 10:46:14 -0500

    Initital commit

---
+++ app/src/main/java/com/heybotler/botlerapp/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.heybotler.botlerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_sign_in);
+    }
+}
