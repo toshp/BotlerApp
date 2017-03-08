fb1802791b8ef00ae7f923df396f5e31134c4516
Toshit Panigrahi <topan@bu.edu>
2017-03-07 20:06:49 -0500

    HashMap caching for bitmap icons

--- app/src/main/java/com/heybotler/botlerapp/helpers/ImageLoadTask.java
+++ app/src/main/java/com/heybotler/botlerapp/helpers/ImageLoadTask.java
@@ -8,13 +8,17 @@ import android.widget.ImageView;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 /**
  * Created by toshitpanigrahi on 3/7/17.
  * SRC http://stackoverflow.com/questions/18953632/how-to-set-image-from-url-for-imageview
+ * MODIFIED to implement a basic HashMap bitmap cache
  */
 
 public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+    // Static cache for icons
+    private static final HashMap<String, Bitmap> iconCache = new HashMap<String, Bitmap>();
 
     private String url;
     private ImageView imageView;
@@ -24,12 +28,25 @@ public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
         this.imageView = imageView;
     }
 
+    /**
+     * Checks the iconCache before execution
+     * and cancels if hit
+     */
+    @Override
+    protected void onPreExecute() {
+        Bitmap icon = iconCache.get(url);
+        if (icon != null) {
+            imageView.setImageBitmap(icon);
+            cancel(true);
+        }
+    }
+
     @Override
     protected Bitmap doInBackground(Void... params) {
         try {
             URL urlConnection = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) urlConnection
-                    .openConnection();
+            HttpURLConnection connection =
+                    (HttpURLConnection) urlConnection.openConnection();
             System.out.println("GETTING IMAGE FROM " + url);
             connection.setDoInput(true);
             connection.connect();
@@ -45,6 +62,9 @@ public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap result) {
         super.onPostExecute(result);
+        // Cache icon for future
+        iconCache.put(url, result);
+        // Set imageView
         imageView.setImageBitmap(result);
     }
 
